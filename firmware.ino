70a326619a06429d93242cbefc99fb8b34ef97ed
sai-pujitha-v <sv9901@srmist.edu.in>
2026-02-15 03:59:54 +0530

    feat: implement interrupt-driven pulse counting and WiFi telemetry

--- firmware.ino
+++ firmware.ino
@@ -1,29 +1,60 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-const int mqPin = 34;
-String ssid = ""; String pass = "";
+#define GM_PIN D2      
+#define BUZ_PIN D5    
+#define LOG_PERIOD 15000 
+
+volatile unsigned long pulseCount = 0; 
+unsigned long lastMillis = 0;
+const float J305_FACTOR = 0.0057; 
+
+void IRAM_ATTR handle_pulse() {
+  pulseCount++;
+}
 
 void setup() {
   Serial.begin(115200);
-  pinMode(mqPin, INPUT);
-  // WiFi provisioning logic from previous projects
+  pinMode(GM_PIN, INPUT_PULLUP);
+  pinMode(BUZ_PIN, OUTPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(GM_PIN), handle_pulse, FALLING);
+
+  WiFi.begin("SSID", "PASS");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nRadiation Monitor Online.");
 }
 
 void loop() {
-  int rawValue = analogRead(mqPin);
-  float voltage = rawValue * (3.3 / 4095.0);
-  
-  // Basic PPM estimation logic
-  float ppm = (voltage / 3.3) * 2000; 
+  unsigned long currentMillis = millis();
 
-  HTTPClient http;
-  http.begin("http://your-aqi-app.com/update");
-  http.addHeader("Content-Type", "application/json");
-  
-  String json = "{\"ppm\":" + String(ppm) + "}";
-  http.POST(json);
-  http.end();
-  
-  delay(5000);
+  if (pulseCount > 0) {
+    digitalWrite(BUZ_PIN, HIGH);
+    delayMicroseconds(50); 
+    digitalWrite(BUZ_PIN, LOW);
+  }
+
+  if (currentMillis - lastMillis >= LOG_PERIOD) {
+    float cpm = pulseCount * (60000.0 / LOG_PERIOD);
+    float dose = cpm * J305_FACTOR;
+
+    Serial.print("CPM: "); Serial.print(cpm);
+    Serial.print(" | Dose: "); Serial.println(dose);
+
+    if (WiFi.status() == WL_CONNECTED) {
+      WiFiClient client;
+      HTTPClient http;
+      http.begin(client, "http://YOUR_LOCAL_IP:8501/update");
+      http.addHeader("Content-Type", "application/json");
+      String json = "{\"cpm\":" + String(cpm) + ",\"usvh\":" + String(dose) + "}";
+      http.POST(json);
+      http.end();
+    }
+
+    pulseCount = 0;
+    lastMillis = currentMillis;
+  }
 }
